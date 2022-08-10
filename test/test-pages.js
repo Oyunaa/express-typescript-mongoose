0658d380ffd834030b120985c8f056ffbbf6875b
Oyunjargal <oyuka579@gmail.com>
2022-08-10 13:11:10 +0800

    test 42

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,6 +1,6 @@
 const expect = require("chai").expect;
 const request = require("request");
-let base_url = "http://localhost:3030";
+let base_url = "http://localhost:3000";
 //"http://localhost:3030";
 
 it("Main page content", function (done) {
