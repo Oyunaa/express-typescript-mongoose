34558f017d2a7af299b7c633f63c0ffa748019c2
Oyunjargal <oyuka579@gmail.com>
2022-08-10 12:16:27 +0800

    testlegdsen

--- test/test-pages.js
+++ test/test-pages.js
@@ -4,10 +4,10 @@ let base_url = "http://localhost:3030";
 
 it("Main page content", function (done) {
   request(`${base_url}/api`, function (error, response, body) {
-    console.log(response);
+    console.log(response.body);
     // expect(body).to.equal("Welcome to REST API");
-    expect(response).to.have.property("body");
-    // expect(body).to.equal("Welcome to REST API");
+    // expect(response).to.have.property("body");
+    expect(body).to.deep.equal("Welcome to REST API");
     done();
   });
 });
