fa9ef5eabbba39ef3d39682af928658f3b94a7d0
Oyunjargal <oyuka579@gmail.com>
2022-08-10 12:51:28 +0800

    test 39

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,6 +1,7 @@
 const expect = require("chai").expect;
 const request = require("request");
-let base_url = "http://localhost:3030";
+let base_url = "http://localhost:2376";
+//"http://localhost:3030";
 
 it("Main page content", function (done) {
   request(`${base_url}/api`, function (error, response, body) {
